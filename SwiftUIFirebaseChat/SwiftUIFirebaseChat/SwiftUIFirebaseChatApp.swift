01b42e109d2320feba8a95daaaf95996a315e197
brad <gjrjs31@naver.com>
2023-07-04 23:16:19 +0900

    feat: 프로젝트 생성 및 타켓 버전 설정

---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by brad on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
